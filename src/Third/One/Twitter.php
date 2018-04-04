09af5731548e972e64f9318979232ae3d4445b35
wenhua <wenhua@qq.com>
2018-04-04 10:39:04 +0800

    init && dev package

---
+++ src/Third/One/Twitter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crady_yang
+ * Date: 2018/4/4
+ * Time: 上午10:30
+ */
+
+namespace Third\One;
+
+class Twitter
+{
+    public function desc()
+    {
+        echo 'This is Twitter Class';
+    }
+}
---
+++ src/Third/Two/Facebook.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crady_yang
+ * Date: 2018/4/4
+ * Time: 上午10:30
+ */
+
+namespace Third\Two;
+
+class Facebook
+{
+    public function desc()
+    {
+        echo 'This is Facebook Class';
+    }
+}
